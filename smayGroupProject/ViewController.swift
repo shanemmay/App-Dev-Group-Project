23e2dff04e6e1ea87b39017eb1d4be99d070cc19
shanemmay <shanemarvinmay@hotmail.com>
2018-11-06 14:36:03 -0600

    Initial Commit

---
+++ smayGroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smayGroupProject
+//
+//  Created by Sheldon May Gunlogson on 11/6/18.
+//  Copyright © 2018 Shane May. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
